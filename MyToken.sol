18ab633ec9f0e1e6d1b37de5dddd92ef5d4a99e0
kennethdelosreyes1 <134952700+kennethdelosreyes1@users.noreply.github.com>
2023-07-28 10:36:14 +0800

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -18,25 +18,24 @@ pragma solidity 0.8.18;
 contract MyToken {
 
     // public variables here
-       string public tokenName = "META";
-       string public tokenAbbrv = "MTA";
-       uint public totalSupply = 0;
-       
+   string public tokenName = "META";
+   string public tokenAbbrv = "MTA";
+   uint public totalSupply = 0;
+
     // mapping variable here
-    mapping(adress => uint) public balances;
+   mapping(address => uint) public balances;
 
     // mint function
-    function mint (address _address, uint _value) public {
-           totalSupply += _value;
-           balances[_address] += _value;
-    }
-    
-    // burn function
-       function burn (address _address, uint _value) public {
-       if (balances[_address] >= _value) {
-           totalSupply -= _value;
-           balances[_address] -= _value;
-    }
-    
+   function mint (address _address, uint _value) public {
+      totalSupply += _value;
+      balances[_address] += _value;
+   }
 
+    // burn function
+   function burn (address _address, uint _value) public {
+      if (balances[_address] >= _value) {
+         totalSupply -= _value;
+         balances[_address] -= _value;
+      }
+   }
 }
